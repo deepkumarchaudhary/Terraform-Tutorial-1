cce601547b34d9a28c0c1313f79f3974de1bff43
Kumar, Deepak <Deepak.kumar0@experian.com>
2020-07-16 16:17:38 +0530

    backend script update

--- terraform-remote-state/backend.tf
+++ terraform-remote-state/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">= 0.11.0"
   backend "s3" {
-    bucket = "backup-state-terraform"
+    bucket = "backup-state-terraform1"
     key    = "terraform/test"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "backend-test"
   }
 }

dc33b4569a0a0b1c5771e0d9c9f2be0e72116d8f
Kumar, Deepak <Deepak.kumar0@experian.com>
2020-08-09 02:13:32 +0530

    update

--- terraform-aws-3tierArch/public_subnets.tf
+++ terraform-aws-3tierArch/public_subnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public_1" {
   vpc_id     = aws_vpc.vpc_3Tier.id
   map_public_ip_on_launch = true
   cidr_block = "10.0.1.0/24"
-  availability_zone = var.availability_zone.a
+  availability_zone = "ap-south-1a"
 
   tags = {
     Name = "public_1-3Tier"
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_2" {
   vpc_id     = aws_vpc.vpc_3Tier.id
   map_public_ip_on_launch = true
   cidr_block = "10.0.2.0/24"
-  availability_zone = var.availability_zone.b
+  availability_zone = "ap-south-1b"
   tags = {
     Name = "public_2-3Tier"
   }

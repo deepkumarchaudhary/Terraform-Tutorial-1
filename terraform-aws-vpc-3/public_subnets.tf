0408dbfed205e7dc249e0a57de7071911eff46dd
Kumar, Deepak <Deepak.kumar0@experian.com>
2020-08-28 00:56:41 +0530

    data resource use

---
+++ terraform-aws-vpc-3/public_subnets.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "subnets" {
+  count      = "${length(data.aws_availability_zones.azs.names)}"
+  vpc_id     = "${aws_vpc.vpc_demo.id}"
+  map_public_ip_on_launch = true
+  cidr_block = "${element(var.subnet_cidr,count.index)}"
+
+  tags = {
+    Name = "public_subnet-${count.index+1}"
+  }
+}
---
+++ terraform-aws-vpc-3/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "vpc_demo" {
+  cidr_block                       = var.cidr
+  instance_tenancy                 = var.instance_tenancy
+
+  tags = {
+      Name = var.tags
+    }
+
+}

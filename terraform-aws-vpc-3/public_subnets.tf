faa585e5f008ed58c62c4b5d5f7ba8261fa9566f
Kumar, Deepak <Deepak.kumar0@experian.com>
2020-08-28 01:03:44 +0530

    update assiging subnet in each azs

--- terraform-aws-vpc-3/public_subnets.tf
+++ terraform-aws-vpc-3/public_subnets.tf
@@ -1,5 +1,6 @@
 resource "aws_subnet" "subnets" {
   count      = "${length(data.aws_availability_zones.azs.names)}"
+  availability_zone = "${element(data.aws_availability_zones.azs.names,count.index)}"
   vpc_id     = "${aws_vpc.vpc_demo.id}"
   map_public_ip_on_launch = true
   cidr_block = "${element(var.subnet_cidr,count.index)}"
